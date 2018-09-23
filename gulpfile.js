eb5607e7dbb082d40b3380dee00e880bf21a78ab
work-bition <work-bition@outlook.com>
2018-09-23 19:31:54 +0800

    修改gulpfile.js中的browsersync任务，引入多个基目录 + 在页面引入jquery和semantic的JS文件

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ const gulp = require('gulp'),
    gulp.task('browser-sync', function() {
       browserSync.init({
           server: {
-              baseDir: "./dist/views"
+              baseDir: ["./dist/views", "./dist/semantic-ui", "./node_modules"]
           },
 
           files: "./dist/views/index.html"
