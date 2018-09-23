8ac5193360a3246d5a434843eae5ca6a8b06e4f6
work-bition <work-bition@outlook.com>
2018-09-23 21:51:10 +0800

    修改semantic.json文件来指明想要生成哪些组件 + 修改gulpfile.js文件确保先清理完再构建文件

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@ const gulp = require('gulp'),
       browserSync = require('browser-sync').create()
 
  let watch = require('./semantic/tasks/watch'),
-     build = require('./semantic/tasks/build')
+     build = require('./semantic/tasks/build'),
+     clean = require('./semantic/tasks/clean')
 
    // import semantic UI tasks with custom task names
    //导入semantic UI框架的gulp任务
    gulp.task('watch-ui', watch)
-   //构建semantic UI框架
-   gulp.task('build-ui', build)
+   //清理semantic UI框架生成的目录
+   gulp.task('clean-ui', clean)
+   //构建semantic UI框架（确保清理任务完成后）
+   gulp.task('build-ui', ['clean-ui'], build)
+
+   //确保清理任务完成后再进行构建
+   gulp.task('build', ['clean-ui', 'build-ui'])
 
    // browserSync创建静态服务器并监视文件变化
    gulp.task('browser-sync', function() {
