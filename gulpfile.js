dcb6ffdb1807acdefc9087e2155fc3cb59e1a3b8
work-bition <work-bition@outlook.com>
2018-09-23 16:57:40 +0800

    将semantic UI框架的gulp任务导入到项目的gulpfile.js中

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 const gulp = require('gulp') //添加要执⾏行行的任务
 
-gulp.task('hello', () => console.log('您好') ) //默认要执⾏行行的任务
+ let
+   watch = require('./semantic/tasks/watch'),
+   build = require('./semantic/tasks/build')
 
-  gulp.task('default', ['hello'])
+   // import semantic UI tasks with custom task names
+   //导入semantic UI框架的gulp任务
+   gulp.task('watch-ui', watch);
+   //构建semantic UI框架
+   gulp.task('build-ui', build);
