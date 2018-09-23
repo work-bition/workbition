8645a5b9de7087f5cbaa80016d2f136f0988146c
work-bition <work-bition@outlook.com>
2018-09-23 17:48:09 +0800

    在gulpfile.js中添加browsersync任务 + 创建静态服务器 + 监视index.html变化

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
-const gulp = require('gulp') //添加要执⾏行行的任务
+const gulp = require('gulp'),
+      browserSync = require('browser-sync').create()
 
- let
-   watch = require('./semantic/tasks/watch'),
-   build = require('./semantic/tasks/build')
+ let watch = require('./semantic/tasks/watch'),
+     build = require('./semantic/tasks/build')
 
    // import semantic UI tasks with custom task names
    //导入semantic UI框架的gulp任务
-   gulp.task('watch-ui', watch);
+   gulp.task('watch-ui', watch)
    //构建semantic UI框架
-   gulp.task('build-ui', build);
+   gulp.task('build-ui', build)
+
+   // browserSync创建静态服务器并监视文件变化
+   gulp.task('browser-sync', function() {
+      browserSync.init({
+          server: {
+              baseDir: "./dist/views"
+          },
+
+          files: "./dist/views/index.html"
+      });
+   });
