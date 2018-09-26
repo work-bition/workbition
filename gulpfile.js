bb147212012d706bfb91a520e5733fd3ca8cef4f
work-bition <work-bition@outlook.com>
2018-09-26 17:27:37 +0800

    整合gulp和Babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,48 @@
-const gulp = require('gulp'),
-      browserSync = require('browser-sync').create(),
-      sass        = require('gulp-sass'),
-      reload      = browserSync.reload
+  //导入gulp, browserSync, sass和Babel模块
+  const gulp = require('gulp'),
+        browserSync = require('browser-sync').create(),
+        sass        = require('gulp-sass'),
+        babel       = require('gulp-babel'),
+        reload      = browserSync.reload
 
-const watch = require('./semantic/tasks/watch'),
-      build = require('./semantic/tasks/build'),
-      clean = require('./semantic/tasks/clean')
+  //导入semantic UI框架的gulp任务
+  const watch = require('./semantic/tasks/watch'),
+        build = require('./semantic/tasks/build'),
+        clean = require('./semantic/tasks/clean')
 
-   // import semantic UI tasks with custom task names
-   //导入semantic UI框架的gulp任务
-   gulp.task('watch-ui', watch)
-   //清理semantic UI框架生成的目录
-   gulp.task('clean-ui', clean)
-   //构建semantic UI框架（确保清理任务完成后）
-   gulp.task('build-ui', ['clean-ui'], build)
+  //监视semantic-UI框架的源文件
+  gulp.task('watch-ui', watch)
+  //清理semantic UI框架生成的目录
+  gulp.task('clean-ui', clean)
+  //构建semantic UI框架（确保清理任务完成后）
+  gulp.task('build-ui', ['clean-ui'], build)
+  //确保清理任务完成后再进行构建
+  gulp.task('build', ['clean-ui', 'build-ui'])
 
-   //确保清理任务完成后再进行构建
-   gulp.task('build', ['clean-ui', 'build-ui'])
+  // scss编译后的css将注入到浏览器里实现更新
+  gulp.task('sass', function() {
+    return gulp.src("./develop/styles/**/*.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("./dist/styles"))
+      .pipe(reload({stream: true}))
+  })
 
-   // scss编译后的css将注入到浏览器里实现更新
-   gulp.task('sass', function() {
-       return gulp.src("./develop/styles/**/*.scss")
-           .pipe(sass())
-           .pipe(gulp.dest("./dist/styles"))
-           .pipe(reload({stream: true}))
-   })
+  //使用Babel处理完JS文件后返回流
+  gulp.task('js', () =>
+
+    gulp.src("./develop/scripts/**/*.js")
+
+  		.pipe(babel({
+
+  			presets: ['@babel/env']
+
+  		}))
+
+  		.pipe(gulp.dest("./dist/scripts"))
+
+      .pipe(reload({stream: true}))
+
+   )
 
    // browserSync创建静态服务器并监视文件变化
    gulp.task('browser-sync', function() {
@@ -32,15 +50,18 @@ const watch = require('./semantic/tasks/watch'),
       browserSync.init({
 
           server: {
-
-              baseDir: ["./dist/views", "./dist/styles", "./dist/semantic-ui", "./node_modules"]
-
+              baseDir: ["./dist/views", "./dist/styles", "./dist/scripts", "./dist/semantic-ui", "./node_modules"]
           },
 
           files: ["./dist/views/index.html", "./dist/semantic-ui/**/*"]
+
       })
 
+      //监听sass文件和js文件变动
+      // 添加 browserSync.reload 到任务队列里
+      // 所有的浏览器重载后任务完成
       gulp.watch("./develop/styles/**/*.scss", ['sass'])
+      gulp.watch("./develop/scripts/**/*.js", ['js'])
 
    })
 
