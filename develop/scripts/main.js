9e29fd34fa2a70af41e75056bed734fd0d3979a4
work-bition <work-bition@outlook.com>
2018-10-26 16:25:35 +0800

    设置响应式断点：1265px/995px + 完成搜索框的设计

--- develop/scripts/main.js
+++ develop/scripts/main.js
@@ -1,10 +1,105 @@
+'use strict'
 
-$('#main-nav .left.menu .item').click((event) => {
+/**
+* main navigation
+*/
 
-  $('#main-nav .left.menu .active.item').removeClass('active')
+$('#header .right.menu .ui.category.search.item i.search.icon').click((event) => {
 
-  let element = event.currentTarget
+  /* making search input visible */
 
-  $(element).addClass('active')
+  let search_input = event.currentTarget.parentElement.children[1]
+
+  search_input.style.visibility = 'visible'
+
+  search_input.style.width = '35rem'
+
+  /* making login and register buttons invisible */
+
+  let login_button = $('#header .right.menu .login_button')
+
+  let divider = $('#header .right.menu .divider_item')
+
+  let register_button = $('#header .right.menu .register_button')
+
+  login_button[0].style.display='none'
+
+  divider[0].style.display='none'
+
+  register_button[0].style.display='none'
+
+  /* removing 'link' class via jQuery */
+
+  $(event.currentTarget).removeClass('link')
+
+  /* making the close icon visible */
+
+  $('#header .right.menu .ui.category.search.item i.close.icon')[0].style.display = 'inline-block'
+
+  /* stopping the propagation */
+
+  event.stopPropagation()
+
+
 
 })
+
+$('#header .right.menu .ui.category.search.item i.close.icon').click((event) => {
+
+  /* making search input invisible */
+
+  let search_input = event.currentTarget.parentElement.children[0].children[1]
+
+  search_input.style.visibility = 'hidden'
+
+  search_input.style.width = '0'
+
+  /* making close icon invisible */
+
+  let close_icon = event.currentTarget
+
+  close_icon.style.display = 'none'
+
+  /* making login and register buttons visible */
+
+  let login_button = $('#header .right.menu .login_button')
+
+  let divider = $('#header .right.menu .divider_item')
+
+  let register_button = $('#header .right.menu .register_button')
+
+  login_button[0].style.display='inline-block'
+
+  divider[0].style.display='inline-block'
+
+  register_button[0].style.display='inline-block'
+
+  /* adding 'link' class to search icon via jQuery */
+
+  $('#header .right.menu .ui.category.search.item i.search.icon').addClass('link')
+
+  /* stopping the propagation */
+
+  event.stopPropagation()
+
+})
+
+/* preventing body click event when click on search input */
+
+$('#header .right.menu .ui.category.search.item .prompt').click((event) => {
+
+  /* stopping the propagation */
+
+  event.stopPropagation()
+
+});
+
+
+/* when clicking in the viewport, making the search input invisible */
+
+$('body').click((event) => {
+
+  /* programmatically trigger close icon click event */
+  $('#header .right.menu .ui.category.search.item i.close.icon').click()
+
+});
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,7 +1,64 @@
-"use strict";
+'use strict';
+/**
+* main navigation
+*/
 
-$('#main-nav .left.menu .item').click(function (event) {
-  $('#main-nav .left.menu .active.item').removeClass('active');
-  var element = event.currentTarget;
-  $(element).addClass('active');
+$('#header .right.menu .ui.category.search.item i.search.icon').click(function (event) {
+  /* making search input visible */
+  var search_input = event.currentTarget.parentElement.children[1];
+  search_input.style.visibility = 'visible';
+  search_input.style.width = '35rem';
+  /* making login and register buttons invisible */
+
+  var login_button = $('#header .right.menu .login_button');
+  var divider = $('#header .right.menu .divider_item');
+  var register_button = $('#header .right.menu .register_button');
+  login_button[0].style.display = 'none';
+  divider[0].style.display = 'none';
+  register_button[0].style.display = 'none';
+  /* removing 'link' class via jQuery */
+
+  $(event.currentTarget).removeClass('link');
+  /* making the close icon visible */
+
+  $('#header .right.menu .ui.category.search.item i.close.icon')[0].style.display = 'inline-block';
+  /* stopping the propagation */
+
+  event.stopPropagation();
+});
+$('#header .right.menu .ui.category.search.item i.close.icon').click(function (event) {
+  /* making search input invisible */
+  var search_input = event.currentTarget.parentElement.children[0].children[1];
+  search_input.style.visibility = 'hidden';
+  search_input.style.width = '0';
+  /* making close icon invisible */
+
+  var close_icon = event.currentTarget;
+  close_icon.style.display = 'none';
+  /* making login and register buttons visible */
+
+  var login_button = $('#header .right.menu .login_button');
+  var divider = $('#header .right.menu .divider_item');
+  var register_button = $('#header .right.menu .register_button');
+  login_button[0].style.display = 'inline-block';
+  divider[0].style.display = 'inline-block';
+  register_button[0].style.display = 'inline-block';
+  /* adding 'link' class to search icon via jQuery */
+
+  $('#header .right.menu .ui.category.search.item i.search.icon').addClass('link');
+  /* stopping the propagation */
+
+  event.stopPropagation();
+});
+/* preventing body click event when click on search input */
+
+$('#header .right.menu .ui.category.search.item .prompt').click(function (event) {
+  /* stopping the propagation */
+  event.stopPropagation();
+});
+/* when clicking in the viewport, making the search input invisible */
+
+$('body').click(function (event) {
+  /* programmatically trigger close icon click event */
+  $('#header .right.menu .ui.category.search.item i.close.icon').click();
 });
