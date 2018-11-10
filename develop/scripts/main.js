c1b449686142e58d3b7ef313cbded26d1c580e8d
work-bition <work-bition@outlook.com>
2018-11-10 12:03:03 +0800

    重写search box关闭时的逻辑

--- develop/scripts/main.js
+++ develop/scripts/main.js
@@ -2,117 +2,14 @@
 
   'use strict'
 
-  /**
-  * main navigation
-  */
-  $('#header .right.menu .ui.search.item i.search.icon').click((event) => {
-
-    /** making search input visible **/
-
-    let search_input = $('#header .right.menu input.prompt')[0]
-
-    search_input.style.visibility = 'visible'
-
-    /** making search input get foucs **/
-
-    search_input.focus()
-
-    /** making login and register buttons invisible **/
-
-    let login_button = $('#header .right.menu .login_button')
-
-    let divider = $('#header .right.menu .divider_item')
-
-    let register_button = $('#header .right.menu .register_button')
-
-    login_button[0].style.display='none'
-
-    divider[0].style.display='none'
-
-    register_button[0].style.display='none'
-
-    /** removing 'link' class via jQuery **/
-
-    $(event.currentTarget).removeClass('link')
-
-    /** making the close icon visible **/
-
-    $('#header .right.menu .ui.search.item i.close.icon')[0].style.display = 'inline-block'
-
-    /** adding class to search icon for negative margin **/
-
-    $(event.currentTarget).addClass('negative_mg_lft')
-
-    /** stopping the propagation **/
-
-    event.stopPropagation()
-
-
-
-  })
-  $('#header .right.menu .ui.search.item i.close.icon').click((event) => {
-
-    /* making search input invisible */
-
-    let search_input = $('#header .right.menu input.prompt')[0]
-
-    search_input.style.visibility = 'hidden'
-
-    search_input.style.width = '0'
-
-    /* making close icon invisible */
-
-    let close_icon = event.currentTarget
-
-    close_icon.style.display = 'none'
-
-    /* making login and register buttons visible */
-
-    let login_button = $('#header .right.menu .login_button')
-
-    let divider = $('#header .right.menu .divider_item')
-
-    let register_button = $('#header .right.menu .register_button')
-
-    login_button[0].style.display='inline-block'
-
-    divider[0].style.display='inline-block'
-
-    register_button[0].style.display='inline-block'
-
-    /* adding 'link' class to search icon via jQuery */
-
-    $('#header .right.menu .ui.search.item i.search.icon').addClass('link')
-
-    /* removing class from search icon for negative margin */
-
-    $('#header .right.menu .ui.search.item i.search.icon').removeClass('negative_mg_lft')
-
-    /* stopping the propagation */
-
-    event.stopPropagation()
-
-  })
-  /** preventing the body click event when click on search input **/
-  $('#header .right.menu .ui.search.item .prompt').click((event) => {
-
-    /* stopping the propagation */
-    event.stopPropagation()
-
-  })
-  /** when clicking in the viewport, making the search input invisible **/
-  $('body').click((event) => {
-
-     /* programmatically trigger close icon click event */
-     $('#header .right.menu .ui.search.item i.close.icon').click()
-
-   });
-
-
-
+   /**
+   * main navigation
+   */
+   
    /**
    * main search bar
    */
+
    /** the message that needs to be passed when there's no search result returned **/
    $.fn.search.settings.error.noResults = '抱歉～我们没找到任何能够匹配此搜索结果的课程。'
 
@@ -202,9 +99,125 @@
         /** clear search box input **/
         $('.ui.search .prompt').val('')
 
-       }
+      }
 
      })
+
+   /** when clicking on the search icon, make the search box visible **/
+   $('#header .right.menu .ui.search.item i.search.icon').click((event) => {
+
+     /** making search box visible **/
+
+     let search_input = $('#header .right.menu input.prompt')[0]
+
+     search_input.style.visibility = 'visible'
+
+     /** making search input get foucs **/
+
+     search_input.focus()
+
+     /** making login and register buttons invisible **/
+
+     let login_button = $('#header .right.menu .login_button')
+
+     let divider = $('#header .right.menu .divider_item')
+
+     let register_button = $('#header .right.menu .register_button')
+
+     login_button[0].style.display='none'
+
+     divider[0].style.display='none'
+
+     register_button[0].style.display='none'
+
+     /** removing 'link' class via jQuery **/
+
+     $(event.currentTarget).removeClass('link')
+
+     /** making the close icon visible **/
+
+     $('#header .right.menu .ui.search.item i.close.icon')[0].style.display = 'inline-block'
+
+     /** adding class to search icon for negative margin **/
+
+     $(event.currentTarget).addClass('negative_mg_lft')
+
+     /** stopping the propagation **/
+
+     event.stopPropagation()
+
+
+
+   })
+
+   /** when click on the close icon, closing the search results panels in two steps **/
+   $('#header .right.menu .ui.search.item i.close.icon').click((event) => {
+
+     /** if search results panel is open, just close it and do nothing else **/
+     if ($('.ui.search').search('is visible')) {
+
+       $('.ui.search').search('hide results')
+
+     }
+
+     /** if search results panel is not open, make the search box invisble **/
+     else {
+
+       /* making search input invisible */
+
+       let search_input = $('#header .right.menu input.prompt')[0]
+
+       search_input.style.visibility = 'hidden'
+
+       search_input.style.width = '0'
+
+       /* making close icon invisible */
+
+       let close_icon = event.currentTarget
+
+       close_icon.style.display = 'none'
+
+       /* making login and register buttons visible */
+
+       let login_button = $('#header .right.menu .login_button')
+
+       let divider = $('#header .right.menu .divider_item')
+
+       let register_button = $('#header .right.menu .register_button')
+
+       login_button[0].style.display='inline-block'
+
+       divider[0].style.display='inline-block'
+
+       register_button[0].style.display='inline-block'
+
+       /* adding 'link' class to search icon via jQuery */
+       $('#header .right.menu .ui.search.item i.search.icon').addClass('link')
+
+       /* removing class from search icon for negative margin */
+       $('#header .right.menu .ui.search.item i.search.icon').removeClass('negative_mg_lft')
+
+     }
+
+   })
+
+   /** preventing the body click event when click on search input **/
+   $('#header .right.menu .ui.search.item .prompt').click((event) => {
+
+     /* stopping the propagation */
+     event.stopPropagation()
+
+   })
+
+   /** when clicking in the viewport, making the search input get focus **/
+   $('body').click((event) => {
+
+     /** making search input get foucs **/
+     let search_input = $('#header .right.menu input.prompt')[0]
+
+     search_input.focus()
+
+    })
 
    /** hiding results panel of the search box  when clicking on it **/
    $('.ui.search .results').click((event) => {
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -6,73 +6,6 @@
   * main navigation
   */
 
-  $('#header .right.menu .ui.search.item i.search.icon').click(function (event) {
-    /** making search input visible **/
-    var search_input = $('#header .right.menu input.prompt')[0];
-    search_input.style.visibility = 'visible';
-    /** making search input get foucs **/
-
-    search_input.focus();
-    /** making login and register buttons invisible **/
-
-    var login_button = $('#header .right.menu .login_button');
-    var divider = $('#header .right.menu .divider_item');
-    var register_button = $('#header .right.menu .register_button');
-    login_button[0].style.display = 'none';
-    divider[0].style.display = 'none';
-    register_button[0].style.display = 'none';
-    /** removing 'link' class via jQuery **/
-
-    $(event.currentTarget).removeClass('link');
-    /** making the close icon visible **/
-
-    $('#header .right.menu .ui.search.item i.close.icon')[0].style.display = 'inline-block';
-    /** adding class to search icon for negative margin **/
-
-    $(event.currentTarget).addClass('negative_mg_lft');
-    /** stopping the propagation **/
-
-    event.stopPropagation();
-  });
-  $('#header .right.menu .ui.search.item i.close.icon').click(function (event) {
-    /* making search input invisible */
-    var search_input = $('#header .right.menu input.prompt')[0];
-    search_input.style.visibility = 'hidden';
-    search_input.style.width = '0';
-    /* making close icon invisible */
-
-    var close_icon = event.currentTarget;
-    close_icon.style.display = 'none';
-    /* making login and register buttons visible */
-
-    var login_button = $('#header .right.menu .login_button');
-    var divider = $('#header .right.menu .divider_item');
-    var register_button = $('#header .right.menu .register_button');
-    login_button[0].style.display = 'inline-block';
-    divider[0].style.display = 'inline-block';
-    register_button[0].style.display = 'inline-block';
-    /* adding 'link' class to search icon via jQuery */
-
-    $('#header .right.menu .ui.search.item i.search.icon').addClass('link');
-    /* removing class from search icon for negative margin */
-
-    $('#header .right.menu .ui.search.item i.search.icon').removeClass('negative_mg_lft');
-    /* stopping the propagation */
-
-    event.stopPropagation();
-  });
-  /** preventing the body click event when click on search input **/
-
-  $('#header .right.menu .ui.search.item .prompt').click(function (event) {
-    /* stopping the propagation */
-    event.stopPropagation();
-  });
-  /** when clicking in the viewport, making the search input invisible **/
-
-  $('body').click(function (event) {
-    /* programmatically trigger close icon click event */
-    $('#header .right.menu .ui.search.item i.close.icon').click();
-  });
   /**
   * main search bar
   */
@@ -163,6 +96,82 @@
       /** clear search box input **/
       $('.ui.search .prompt').val('');
     }
+  });
+  /** when clicking on the search icon, make the search box visible **/
+
+  $('#header .right.menu .ui.search.item i.search.icon').click(function (event) {
+    /** making search box visible **/
+    var search_input = $('#header .right.menu input.prompt')[0];
+    search_input.style.visibility = 'visible';
+    /** making search input get foucs **/
+
+    search_input.focus();
+    /** making login and register buttons invisible **/
+
+    var login_button = $('#header .right.menu .login_button');
+    var divider = $('#header .right.menu .divider_item');
+    var register_button = $('#header .right.menu .register_button');
+    login_button[0].style.display = 'none';
+    divider[0].style.display = 'none';
+    register_button[0].style.display = 'none';
+    /** removing 'link' class via jQuery **/
+
+    $(event.currentTarget).removeClass('link');
+    /** making the close icon visible **/
+
+    $('#header .right.menu .ui.search.item i.close.icon')[0].style.display = 'inline-block';
+    /** adding class to search icon for negative margin **/
+
+    $(event.currentTarget).addClass('negative_mg_lft');
+    /** stopping the propagation **/
+
+    event.stopPropagation();
+  });
+  /** when click on the close icon, closing the search results panels in two steps **/
+
+  $('#header .right.menu .ui.search.item i.close.icon').click(function (event) {
+    /** if search results panel is open, just close it and do nothing else **/
+    if ($('.ui.search').search('is visible')) {
+      $('.ui.search').search('hide results');
+    }
+    /** if search results panel is not open, make the search box invisble **/
+    else {
+        /* making search input invisible */
+        var search_input = $('#header .right.menu input.prompt')[0];
+        search_input.style.visibility = 'hidden';
+        search_input.style.width = '0';
+        /* making close icon invisible */
+
+        var close_icon = event.currentTarget;
+        close_icon.style.display = 'none';
+        /* making login and register buttons visible */
+
+        var login_button = $('#header .right.menu .login_button');
+        var divider = $('#header .right.menu .divider_item');
+        var register_button = $('#header .right.menu .register_button');
+        login_button[0].style.display = 'inline-block';
+        divider[0].style.display = 'inline-block';
+        register_button[0].style.display = 'inline-block';
+        /* adding 'link' class to search icon via jQuery */
+
+        $('#header .right.menu .ui.search.item i.search.icon').addClass('link');
+        /* removing class from search icon for negative margin */
+
+        $('#header .right.menu .ui.search.item i.search.icon').removeClass('negative_mg_lft');
+      }
+  });
+  /** preventing the body click event when click on search input **/
+
+  $('#header .right.menu .ui.search.item .prompt').click(function (event) {
+    /* stopping the propagation */
+    event.stopPropagation();
+  });
+  /** when clicking in the viewport, making the search input get focus **/
+
+  $('body').click(function (event) {
+    /** making search input get foucs **/
+    var search_input = $('#header .right.menu input.prompt')[0];
+    search_input.focus();
   });
   /** hiding results panel of the search box  when clicking on it **/
 
