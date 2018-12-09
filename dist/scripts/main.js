c55c1521e7ff3df5e902bbf84e0feb0324341c1e
work-bition <work-bition@outlook.com>
2018-12-09 22:45:43 +0800

    添加moveNavButtons_setOverlay函数（减少重复js代码）/ 在小于等于991px像素时，使右箭头到左侧的距离正确

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -387,74 +387,32 @@
   });
   /** Fix the problem that the arrows doesn't fade out, **/
 
-  /** When the mouse hovers on the navigation dots and left and right arrow, stop it from moving **/
-
-  /** and make the color of the overlay on the slick slides lighter **/
-
-  $('.featured_carousel .prev.button, .featured_carousel .next.button, .featured_carousel .slick-dots').on('mouseover', function (event) {
-    $('.featured_carousel .prev.button').css({
-      'transform': 'translate(1.2rem, -50%)',
-      'opacity': '1'
-    });
-    $('.featured_carousel .next.button').css({
-      'transform': 'translate(-0.4rem, -50%)',
-      'opacity': '1'
-    });
-    $('.featured_carousel .slick-list .slick-slide .image_holder .overlay').css({
-      'background-color': 'rgba(0,0,0,0.05)'
-    });
-  });
-  /** Fix the problem that the arrows doesn't fade out, **/
-
-  /** when the mouse moves out of the navigation dots and the left border and the right border of the carousel very quickly **/
-
-  /** and make the color of the overlay on the slick slides darker **/
-
-  $('.featured_carousel .prev.button, .featured_carousel .next.button, .featured_carousel .slick-dots').on('mouseleave', function (event) {
-    $('.featured_carousel .prev.button').css({
-      'transform': 'translate(0.4rem, -50%)',
-      'opacity': '0'
-    });
-    $('.featured_carousel .next.button').css({
-      'transform': 'translate(0.4rem, -50%)',
-      'opacity': '0'
-    });
-    $('.featured_carousel .slick-list .slick-slide .image_holder .overlay').css({
-      'background-color': 'rgba(0,0,0,0.2)'
-    });
-  });
-  /** When the mouse hovers on the carousel area, making the left and right arrows showing out there with fade animation effects **/
+  /** when the mouse hovers on the carousel area, navigation dots and left and right arrows, making the left and right arrows showing out there with fade animation effects **/
 
   /** and make the color of the overlay on the slick slides change between lighter and darker **/
 
-  $('.featured_carousel .slick-list').hover(
-  /** When the mouse enters into the carousel area, making the arrows fade in **/
-  function () {
+  function moveNavButtons_setOverlay(disToMove, buttonOpacity, overlayOpacity) {
     $('.featured_carousel .prev.button').css({
-      'transform': 'translate(1.2rem, -50%)',
-      'opacity': '1'
+      'transform': "translate(".concat(disToMove, ", -50%)"),
+      'opacity': "".concat(buttonOpacity)
     });
     $('.featured_carousel .next.button').css({
-      'transform': 'translate(-0.4rem, -50%)',
-      'opacity': '1'
+      'transform': "translate(-".concat(disToMove, ", -50%)"),
+      'opacity': "".concat(buttonOpacity)
     });
     $('.featured_carousel .slick-list .slick-slide .image_holder .overlay').css({
-      'background-color': 'rgba(0,0,0,0.05)'
+      'background-color': "rgba(0,0,0,".concat(overlayOpacity, ")")
     });
+  }
+
+  $('.featured_carousel .slick-list, .featured_carousel .prev.button, .featured_carousel .next.button, .featured_carousel .slick-dots').hover(
+  /** When the mouse enters into the carousel area, making the arrows fade in **/
+  function () {
+    moveNavButtons_setOverlay('1.2rem', '1', '0.05');
   },
   /** When the mouse leaves the carousel area, making the arrows fade out **/
   function () {
-    $('.featured_carousel .prev.button').css({
-      'transform': 'translate(0.4rem, -50%)',
-      'opacity': '0'
-    });
-    $('.featured_carousel .next.button').css({
-      'transform': 'translate(0.4rem, -50%)',
-      'opacity': '0'
-    });
-    $('.featured_carousel .slick-list .slick-slide .image_holder .overlay').css({
-      'background-color': 'rgba(0,0,0,0.2)'
-    });
+    moveNavButtons_setOverlay('0.4rem', '0', '0.2');
   });
   /**
   * Page Banners - Corner Banners
